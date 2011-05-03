145a9ed7a4ee93e9f55981ab34760dcdcb0fea99
Ryan L. Cross <rcross@cylence.com>
2011-05-02 18:19:43 -0600

    Added per-season progress to the Shows#show view.

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,5 +1,12 @@
 module ShowsHelper
-  def progress(show)
-    (current_user.episodes.find_all_by_show_id(show.id).size.to_f / show.episodes.size.to_f * 100).round
+  def progress(show, season=nil)
+    unless season
+      (current_user.episodes.find_all_by_show_id(show.id).size.to_f / show.episodes.size.to_f * 100).round
+    else
+      total_episodes = show.episodes.select{ |e| e.season == season }.size.to_f
+      watched_episodes = current_user.episodes.find_all_by_show_id(show.id).select{ |e| e.season == season }.size.to_f
+      (watched_episodes / total_episodes * 100).round
+    end
   end
+
 end
