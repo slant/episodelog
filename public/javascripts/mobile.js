12a675cfd84612443a0ba65f56dbfc4186555a99
Ryan L. Cross <rcross@cylence.com>
2011-05-11 09:18:29 -0600

    Added header to mobile dashboard

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -1,11 +1,12 @@
 $(function(){
-  $('.search_button').toggle(
-    function(){
+  $('.search_button').bind('click', function(){
+    if ($('#search_box').is(':visible')){
+      // TODO: We will eventually want this to simply slide back up if
+      // no search is in progress, not reload the page
+      $('#search_box').slideUp('fast');
+    } else {
       $('#search_box').slideDown('fast');
       return false;
-    }, function(){
-      $('#search_box').slideUp('fast');
-      return false;
     }
-  );
+  });
 });
