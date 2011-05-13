2b0318550b45ba8dd9ea99329ddec76444f5dd2d
Ryan L. Cross <rcross@cylence.com>
2011-05-13 01:59:42 -0600

    Finished stubbing the views for the iPhone and added a bunch fo functionality to the views.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,5 +8,19 @@ class HomeController < ApplicationController
 
   def dashboard
     @shows = current_user.shows.order('name asc')
+
+    # Get all shows user has watched to any extent
+    @watched_shows = Show.find(current_user.episodes.collect(&:show_id).uniq)
+
+    # Find shows completed by user
+    @completed_shows = []
+    @watched_shows.each do |show|
+      if show.episodes.size == current_user.episodes.where( :show_id => show.id ).size
+        @completed_shows << show
+      end
+    end
+    
+    @shows.reject! { |s| @completed_shows.include? s }
+    @watched_shows.reject! { |s| @shows.include?(s) or @completed_shows.include?(s) }
   end
 end
