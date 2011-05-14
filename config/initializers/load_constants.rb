7917fafcd1fd0fb23c5f67dce4da93ea5e8bf0db
Ryan L. Cross <rcross@cylence.com>
2011-05-13 19:48:22 -0600

    Added new shows.

--- config/initializers/load_constants.rb
+++ config/initializers/load_constants.rb
@@ -1,10 +1,13 @@
 URLS = [
   'http://www.tv.com/30-rock/show/58326', # 30 Rock
+  'http://www.tv.com/alias/show/3451', # Alias
   'http://www.tv.com/the-amazing-race/show/6686', # The Amazing Race
   'http://www.tv.com/archer/show/77555', # Archer
   'http://www.tv.com/battlestar-galactica-2003/show/23557', # Battlestar Galactica
   'http://www.tv.com/the-big-bang-theory/show/58056', # The Big Bang Theory
+  'http://www.tv.com/bones/show/33332', # Bones
   'http://www.tv.com/chuck/show/68724', # Chuck
+  'http://www.tv.com/desperate-housewives/show/24641', # Desperate Housewives
   'http://www.tv.com/doctor-who/show/34391', # Doctor Who
   'http://www.tv.com/doctor-who-1963/show/355', # Doctor Who (1963)
   'http://www.tv.com/the-event-/show/78485', # The Event
@@ -13,10 +16,12 @@ URLS = [
   'http://www.tv.com/flight-of-the-conchords/show/68719', # Flight of the Conchords
   'http://www.tv.com/friends/show/71', # Friends
   'http://www.tv.com/fringe/show/75146', # Fringe
+  'http://www.tv.com/glee/show/75988', # Glee
   'http://www.tv.com/greys-anatomy/show/24440', # Grey's Anatomy
   'http://www.tv.com/house/show/22374', # House
   'http://www.tv.com/how-i-met-your-mother/show/33700', # How I Met Your Mother
   'http://www.tv.com/the-it-crowd-/show/54188', # The IT Crowd
+  'http://www.tv.com/lie-to-me/show/75671', # Lie to Me
   'http://www.tv.com/lost/show/24313', # Lost
   'http://www.tv.com/the-middle/show/68657', # Middle, The
   'http://www.tv.com/ncis/show/16772', # NCIS
@@ -27,6 +32,9 @@ URLS = [
   'http://www.tv.com/seinfeld/show/112', # Seinfeld
   'http://www.tv.com/the-simpsons/show/146', # The Simpsons
   'http://www.tv.com/south-park/show/344', # South Park
+  'http://www.tv.com/stargate-atlantis/show/11415', # Stargate Atlantis
+  'http://www.tv.com/stargate-sg-1/show/185', # Stargate SG-1
+  'http://www.tv.com/stargate-universe/show/76070', # Stargate Universe
   'http://www.tv.com/star-trek/show/633', # Star Trek
   'http://www.tv.com/star-trek-deep-space-nine/show/166', # Star Trek: Deep Space Nine
   'http://www.tv.com/star-trek-enterprise/show/2498', # Star Trek: Enterprise
@@ -34,6 +42,7 @@ URLS = [
   'http://www.tv.com/star-trek-voyager/show/274', # Star Trek: Voyager Season
   'http://www.tv.com/supernatural/show/30144', # Supernatural
   'http://www.tv.com/survivor/show/4742', # Survivor
+  'http://www.tv.com/threes-company/show/629', # Three's Company
   'http://www.tv.com/torchwood/show/50470', # Torchwood
   'http://www.tv.com/the-x-files/show/61' # The X-Files
 ]
