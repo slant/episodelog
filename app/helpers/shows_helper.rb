f08329d0cb145038d360b0ca7939e964abe8c7ad
Ryan L. Cross <rcross@cylence.com>
2011-05-15 01:32:45 -0600

    Progress bars now reflect only episodes that have already aired.

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,7 +1,7 @@
 module ShowsHelper
   def progress(show, season=nil)
     unless season
-      (current_user.episodes.find_all_by_show_id(show.id).size.to_f / show.episodes.size.to_f * 100).round
+      (current_user.episodes.find_all_by_show_id(show.id).reject{ |e| e.air_date ? e.air_date > Date.today : false }.size.to_f / show.episodes.size.to_f * 100).round
     else
       total_episodes = show.episodes.select{ |e| e.season == season }.size.to_f
       watched_episodes = current_user.episodes.find_all_by_show_id(show.id).select{ |e| e.season == season }.size.to_f
