dddd078e93f37b3c268a6d49b55a644b0358f57e
Ryan L. Cross <rcross@cylence.com>
2011-05-23 20:05:15 -0600

    Moved the progress bars into a partial. Corrected an issue where the completion percentage was being calculated from all episodes listed for a show instead of all aired episodes.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,12 +15,14 @@ class HomeController < ApplicationController
     # Find shows completed by user
     @completed_shows = []
     @watched_shows.each do |show|
-      if show.episodes.size == current_user.episodes.where( :show_id => show.id ).size
+      if show.episodes.reject{ |e| e.air_date ? e.air_date > Date.today : false }.size == current_user.episodes.where( :show_id => show.id ).reject{ |e| e.air_date ? e.air_date > Date.today : false }.size
         @completed_shows << show
       end
     end
-    
+
     @shows.reject! { |s| @completed_shows.include? s }
     @watched_shows.reject! { |s| @shows.include?(s) or @completed_shows.include?(s) }
+  
+    @show_count = @shows.size + @watched_shows.size + @completed_shows.size
   end
 end
