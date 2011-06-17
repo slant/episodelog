85981c8216f804c574ac7320bbd9332407c970d5
Ryan L. Cross <rcross@cylence.com>
2011-06-16 23:04:40 -0600

    Added more shows. Added a new background image.

--- config/initializers/load_constants.rb
+++ config/initializers/load_constants.rb
@@ -1,13 +1,17 @@
 URLS = [
   'http://www.tv.com/30-rock/show/58326', # 30 Rock
+  'http://www.tv.com/the-a-team/show/727', # The A-Team
   'http://www.tv.com/alias/show/3451', # Alias
   'http://www.tv.com/the-amazing-race/show/6686', # The Amazing Race
   'http://www.tv.com/archer/show/77555', # Archer
   'http://www.tv.com/battlestar-galactica-2003/show/23557', # Battlestar Galactica
   'http://www.tv.com/the-big-bang-theory/show/58056', # The Big Bang Theory
+  'http://www.tv.com/blue-bloods/show/78706', # Blue Bloods
   'http://www.tv.com/bones/show/33332', # Bones
+  'http://www.tv.com/burn-notice/show/69107', # Burn Notice
   'http://www.tv.com/chuck/show/68724', # Chuck
   'http://www.tv.com/community/show/76841', # Community
+  'http://www.tv.com/dark-angel/show/36', # Dark Angel
   'http://www.tv.com/desperate-housewives/show/24641', # Desperate Housewives
   'http://www.tv.com/doctor-who/show/34391', # Doctor Who
   'http://www.tv.com/doctor-who-1963/show/355', # Doctor Who (1963)
@@ -16,25 +20,37 @@ URLS = [
   'http://www.tv.com/firefly/show/7097', # Firefly
   'http://www.tv.com/flashforward/show/76455', # FlashForward
   'http://www.tv.com/flight-of-the-conchords/show/68719', # Flight of the Conchords
+  'http://www.tv.com/friday-night-lights/show/58137', # Friday Night Lights
   'http://www.tv.com/friends/show/71', # Friends
   'http://www.tv.com/fringe/show/75146', # Fringe
   'http://www.tv.com/game-of-thrones/show/77121', # Game of Thrones
   'http://www.tv.com/glee/show/75988', # Glee
   'http://www.tv.com/greys-anatomy/show/24440', # Grey's Anatomy
+  'http://www.tv.com/happy-days/show/270', # Happy Days
   'http://www.tv.com/house/show/22374', # House
   'http://www.tv.com/how-i-met-your-mother/show/33700', # How I Met Your Mother
+  'http://www.tv.com/i-love-lucy/show/1038', # I Love Lucy
   'http://www.tv.com/the-it-crowd-/show/54188', # The IT Crowd
   'http://www.tv.com/justified/show/77651', # Justified
+  'http://www.tv.com/knight-rider/show/74986', # Knight Rider
   'http://www.tv.com/lie-to-me/show/75671', # Lie to Me
   'http://www.tv.com/lost/show/24313', # Lost
+  'http://www.tv.com/macgyver/show/706', # MacGyver
+  'http://www.tv.com/magnum-p.i./show/267', # Magnum, P.I.
+  'http://www.tv.com/miami-vice/show/544', # Miami Vice
   'http://www.tv.com/the-middle/show/68657', # Middle, The
+  'http://www.tv.com/modern-family/show/77270', # Modern Family
   'http://www.tv.com/ncis/show/16772', # NCIS
   'http://www.tv.com/ncis-los-angeles/show/76607', # NCIS: Los Angeles
   'http://www.tv.com/nikita/show/78642', # Nikita
   'http://www.tv.com/the-office/show/22343', # The Office
+  'http://www.tv.com/parenthood-/show/76680', # Parenthood
   'http://www.tv.com/parks-and-recreation/show/75964', # Parks and Recreation
+  'http://www.tv.com/quantum-leap/show/173', # Quantum Leap
+  'http://www.tv.com/royal-pains/show/75383', # Royal Pains
   'http://www.tv.com/scrubs/show/3613', # Scrubs
   'http://www.tv.com/seinfeld/show/112', # Seinfeld
+  'http://www.tv.com/sherlock-holmes-in-the-22nd-century/show/10302', # Sherlock Holmes in the 22nd Century
   'http://www.tv.com/the-simpsons/show/146', # The Simpsons
   'http://www.tv.com/south-park/show/344', # South Park
   'http://www.tv.com/stargate-atlantis/show/11415', # Stargate Atlantis
@@ -49,8 +65,7 @@ URLS = [
   'http://www.tv.com/survivor/show/4742', # Survivor
   'http://www.tv.com/threes-company/show/629', # Three's Company
   'http://www.tv.com/torchwood/show/50470', # Torchwood
+  'http://www.tv.com/true-blood/show/74645', # True Blood
   'http://www.tv.com/the-walking-dead/show/78582', # Walking Dead
   'http://www.tv.com/the-x-files/show/61' # The X-Files
 ]
-
-# Community, Parks & Rec, Game of Thrones, Justified, Walking Dead
